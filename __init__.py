0dd087b4130a95195a0b851febb4bfc7d7359aef
BobsBlazed <39865461+BobsBlazed@users.noreply.github.com>
2025-05-15 10:58:26 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,25 @@
-from .Bobs_Latent_Optimizer import BobsLatentNode import BobsLatentNodeAdvanced
+# __init__.py
 
-# --- NODE MAPPINGS ---
-NODE_CLASS_MAPPINGS = {
-    "BobsLatentNode": BobsLatentNode,
-    "BobsLatentNodeAdvanced": BobsLatentNodeAdvanced
-}
+"""
+@author: BobsBlazed
+@title: Bobs Latent Optimizer
+@nickname: BobsLatent
+@description: Nodes to generate empty latent images optimized for FLUX, SDXL, and SD3 models.
+"""
 
-# --- NODE DISPLAY NAMES ---
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Bobs-Latent-Optimizer": "Bobs Latent Optimizer",
-    "Bobs-Advanced-Latent-Optimizer": "Bobs Latent Optimizer (Advanced)"
-}
+# Import the node mappings from your main node file
+from .Bobs_Latent_Optimizer import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# A dictionary that contains all nodes you want to be exposed in the UI
+# NOTE: It is usually set to NODE_CLASS_MAPPINGS
+NODE_CLASS_MAPPINGS = NODE_CLASS_MAPPINGS
+
+# A dictionary that contains the friendly/user friendly category names
+NODE_DISPLAY_NAME_MAPPINGS = NODE_DISPLAY_NAME_MAPPINGS
+
+# If you had a separate web directory with frontend JavaScript for your nodes,
+# you would specify it here. Since these nodes seem to use standard ComfyUI widgets,
+# you likely don't need a custom web directory.
+# WEB_DIRECTORY = "./web"
+
+print("✨ Bobs Latent Optimizer nodes loaded! ✨")
