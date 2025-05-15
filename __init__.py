349cfc6a3f80a1ad1f5cd137bafabd11fadf5ddb
BobsBlazed <39865461+BobsBlazed@users.noreply.github.com>
2025-05-15 11:10:28 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,7 @@
-"""
-@author: BobsBlazed
-@title: Bobs Latent Optimizer
-@nickname: BobsLatent
-@description: Nodes to generate empty latent images optimized for FLUX, SDXL, and SD3 models.
-"""
-
-from .Bobs_Latent_Optimizer import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .Bobs_Latent_Optimizer import NODE_CLASS_MAPPINGS
+from .Bobs_Latent_Optimizer import NODE_DISPLAY_NAME_MAPPINGS
 
 NODE_CLASS_MAPPINGS = NODE_CLASS_MAPPINGS
-
-
 NODE_DISPLAY_NAME_MAPPINGS = NODE_DISPLAY_NAME_MAPPINGS
 
 print("✨ Bobs Latent Optimizer nodes loaded! ✨")
