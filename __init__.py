0b0c96a0aa8c38f2e1c25be28c716afe7c501daf
BobsBlazed <39865461+BobsBlazed@users.noreply.github.com>
2025-05-15 10:59:40 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-# __init__.py
-
 """
 @author: BobsBlazed
 @title: Bobs Latent Optimizer
@@ -7,19 +5,11 @@
 @description: Nodes to generate empty latent images optimized for FLUX, SDXL, and SD3 models.
 """
 
-# Import the node mappings from your main node file
 from .Bobs_Latent_Optimizer import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-# A dictionary that contains all nodes you want to be exposed in the UI
-# NOTE: It is usually set to NODE_CLASS_MAPPINGS
 NODE_CLASS_MAPPINGS = NODE_CLASS_MAPPINGS
 
-# A dictionary that contains the friendly/user friendly category names
-NODE_DISPLAY_NAME_MAPPINGS = NODE_DISPLAY_NAME_MAPPINGS
 
-# If you had a separate web directory with frontend JavaScript for your nodes,
-# you would specify it here. Since these nodes seem to use standard ComfyUI widgets,
-# you likely don't need a custom web directory.
-# WEB_DIRECTORY = "./web"
+NODE_DISPLAY_NAME_MAPPINGS = NODE_DISPLAY_NAME_MAPPINGS
 
 print("✨ Bobs Latent Optimizer nodes loaded! ✨")
